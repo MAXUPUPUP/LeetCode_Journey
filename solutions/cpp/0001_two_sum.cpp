27e30f921402d09fe4850707934c46ff0fecc93d
MAXUPUPUP <569977507@qq.com>
2026-04-09 23:02:52 +0900

    0001 两数之和

---
+++ solutions/cpp/0001_two_sum.cpp
@@ -0,0 +1,23 @@
+/*
+LeetCode 1. Two Sum
+Link: https://leetcode.com/problems/two-sum/
+Topic: array/hash-table
+
+Idea:
+- 
+
+Time Complexity: O(?)
+Space Complexity: O(?)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solve(vector<int>& nums) {
+        // TODO: rename method/signature to LeetCode required signature.
+        return 0;
+    }
+};
+
