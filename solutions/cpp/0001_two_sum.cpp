6fe461f5343daefbd7d09db8bd8e00a50d8a82ae
MAXUPUPUP <569977507@qq.com>
2026-04-09 23:09:44 +0900

    fix: update 0001 two sum cpp implementation

--- solutions/cpp/0001_two_sum.cpp
+++ solutions/cpp/0001_two_sum.cpp
@@ -6,8 +6,8 @@ Topic: array/hash-table
 Idea:
 - 
 
-Time Complexity: O(?)
-Space Complexity: O(?)
+Time Complexity: O(n)
+Space Complexity: O(n)
 */
 
 #include <bits/stdc++.h>
@@ -15,9 +15,16 @@ using namespace std;
 
 class Solution {
 public:
-    int solve(vector<int>& nums) {
-        // TODO: rename method/signature to LeetCode required signature.
-        return 0;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> heap;
+        int n = nums.size();
+        for(int i = 0; i < n; i ++){
+            int r = target - nums[i];
+            if(heap.count(r))  return{heap[r], i};
+            heap[nums[i]] = i;
+            //给出的数字作为key,下标是value。heap[key]=value。
+        }
+        return {};
     }
 };
 
